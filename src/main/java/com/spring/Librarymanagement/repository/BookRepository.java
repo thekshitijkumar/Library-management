38402c2eb8b3b34aa35a22de52cf25be30dc4195
Kshitij Kumar <97kshitij@gmail.com>
2021-02-28 10:53:32 +0530

    Added code for findBy query in authorRepository

--- src/main/java/com/spring/Librarymanagement/repository/BookRepository.java
+++ src/main/java/com/spring/Librarymanagement/repository/BookRepository.java
@@ -28,4 +28,6 @@ public interface BookRepository extends JpaRepository<Book,Integer> {
     @Modifying
     @Query("update Book b set b.card=:#{#book.card} , b.available=:#{#book.available} where b.id=:#{#book.id}")
     void updateBook(Book book);
+
+
 }
